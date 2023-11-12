0cabf4f5381d594f0c86076e9c7ed2a27220e23f
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-11-12 19:05:06 +0000

    Add urls
    Added urls to checkout app.

---
+++ checkout/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from . import views as checkout_views
+
+
+urlpatterns = [
+    path('', views.checkout, name='checkout'),
+    path('checkout/success/<str:order_number>/', views.checkout_success, name='checkout_success'),
+    path('stripe-public-key/', checkout_views.stripe_public_key, name='stripe_public_key'),
+
+]
