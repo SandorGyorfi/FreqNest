4da6c8a73576aafbd2f832461b5aec9458a51754
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-11-12 13:54:34 +0000

    Add function to cart
    Added new function to adjust cart.

--- cart/views.py
+++ cart/views.py
@@ -46,3 +46,22 @@ def adjust_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, item_id):
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+
+        cart = request.session.get('cart', {})
+
+        if item_id in cart:
+            del cart[item_id]
+            request.session['cart'] = cart
+
+            messages.success(request, f'{product.name} Removed from your cart')
+
+        return redirect(reverse('view_cart'))  
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
