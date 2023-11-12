09f03b2a8788eb7d66c4f3e512b11b91355a5d34
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-11-12 18:30:58 +0000

    Update models at checkout
    Added OrderLineItem model with save override for totals.

--- checkout/models.py
+++ checkout/models.py
@@ -42,3 +42,20 @@ class Order(models.Model):
 
     def __str__(self):
         return self.order_number
+    
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the lineitem total
+        and update the order total.
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'Product {self.product.name} on order {self.order.order_number}'    
