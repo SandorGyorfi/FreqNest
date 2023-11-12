679a925d8cb76bd4a0a9b471c81b3485bf9af841
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-11-12 18:29:16 +0000

    Updated Order model
    Added total update and save method override.

--- checkout/models.py
+++ checkout/models.py
@@ -22,3 +22,23 @@ class Order(models.Model):
         Generate a random, unique order number using UUID
         """
         return uuid.uuid4().hex.upper()
+    
+    def update_total(self):
+        """
+        Update grand total each time a line item is added.
+        """
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.grand_total = self.order_total
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
