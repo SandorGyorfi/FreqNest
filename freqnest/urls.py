5bafeebf88c469387a0c29213ad82de5d7006603
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-11-12 12:49:59 +0000

    Added urls
    Urls added to existing htmls.

--- freqnest/urls.py
+++ freqnest/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('shop.urls')),
+    path('synths/', include('synths.urls')),
+
 
 ]
