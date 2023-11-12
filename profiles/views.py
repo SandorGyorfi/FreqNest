618e762a6370451e74e3d9f69c8d1baab4c0a244
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-11-12 19:57:03 +0000

    Add views
    Added user profile view with update functionality and order history display.

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserProfileForm
+from .models import Profile
 
-# Create your views here.
+@login_required
+def profile(request):
+    profile = get_object_or_404(Profile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+    else:
+        form = UserProfileForm(instance=profile)
+
+    orders = profile.user.order_set.all()  
+    return render(request, 'profiles/profile.html', {'form': form, 'orders': orders})
