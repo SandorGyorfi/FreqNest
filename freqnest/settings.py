1c4602477fca7aec5ff08ee587470049739d23d6
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-11-12 13:40:24 +0000

    Add new app
    Cart app added to project.

--- freqnest/settings.py
+++ freqnest/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'shop',
     'synths',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
