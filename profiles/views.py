2a5dc7c9bb8eb9b62280a16f4ea94c23f346c43b
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-11-13 08:47:59 +0000

    Update profiles view
    After debug the missmatch fixed in views.

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserProfileForm
@@ -6,15 +6,31 @@ from .models import Profile
 
 @login_required
 def profile(request):
-    profile = get_object_or_404(Profile, user=request.user)
+    try:
+        user_profile = Profile.objects.get(user=request.user)
+    except Profile.DoesNotExist:
+        user_profile = None
+        messages.info(request, "Profile not found. Please create a profile.")
 
-    if request.method == 'POST':
-        form = UserProfileForm(request.POST, instance=profile)
+    if request.method == 'POST' and user_profile:
+        form = UserProfileForm(request.POST, instance=user_profile)
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
+        else:
+            print("Form errors:", form.errors) 
     else:
-        form = UserProfileForm(instance=profile)
+        form = UserProfileForm(instance=user_profile) if user_profile else UserProfileForm()
 
-    orders = profile.user.order_set.all()  
-    return render(request, 'profiles/profiles.html', {'form': form, 'orders': orders})
+    context = {
+        'form': form
+    }
+
+    if user_profile:
+        orders = user_profile.user.order_set.all()
+        print("Orders:", orders)  
+        context['orders'] = orders
+    else:
+        context['orders'] = []
+
+    return render(request, 'profiles/profiles.html', context)
