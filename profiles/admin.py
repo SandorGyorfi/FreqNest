82cbff29fde42c1ffaeffac267f128d39e686101
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-11-13 13:45:19 +0000

    Add to database
    Registered the profiles admin to database.

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'default_phone_number', 'default_street_address1', 'default_street_address2', 'default_town_or_city', 'default_postcode', 'default_country')
+    search_fields = ('user__username',)
+
+admin.site.register(Profile, ProfileAdmin)
