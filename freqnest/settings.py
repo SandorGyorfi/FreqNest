913c909fac703ee336accd5e1b6bc112d179c850
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-11-15 17:31:31 +0000

    Add static file

--- freqnest/settings.py
+++ freqnest/settings.py
@@ -51,7 +51,6 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
     'shop',
     'synths',
     'cart',
