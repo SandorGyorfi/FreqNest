44d98602c36e9bdb1b03bca3783e3046e182e911
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-11-16 20:35:10 +0000

    Add s3
    Aws added properly to project.

--- freqnest/settings.py
+++ freqnest/settings.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = [
     'checkout',
     'stripe',
     'profiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -176,6 +177,7 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 # Static files configuration
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Media files configuration
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
