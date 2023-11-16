9d92819e4e24ca3768eaf2311c0c7563312513ce
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-11-16 19:13:32 +0000

    Add media
    Media added to S3.

--- freqnest/settings.py
+++ freqnest/settings.py
@@ -168,6 +168,20 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'freqnest-images'
+AWS_S3_REGION_NAME = 'us-east-2'  
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+# Static files configuration
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Media files configuration
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+
 # Storage for session messages
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
