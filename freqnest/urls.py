86963455a672300f2e672524b0112b55d655b2b1
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-11-17 13:18:54 +0000

    Update Prev
    Corrected previous update.

--- freqnest/urls.py
+++ freqnest/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import custom_error_view  
 
 
 
@@ -34,4 +33,3 @@ urlpatterns = [
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
-handler500 = custom_error_view
