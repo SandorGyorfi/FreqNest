ce879722c6f311f664264fd789d835238e82d491
Sandor Gyorfi <mr.sandorgyorfi@gmail.com>
2023-11-20 15:00:31 +0000

    Updated Settings for heroku deployement

--- custom_storages.py
+++ custom_storages.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
@@ -9,5 +8,4 @@ class StaticStorage(S3Boto3Storage):
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
-
     
